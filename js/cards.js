586c25ecace71b4252b4ea234426c7b1bab9df21
mshns <mishanos412@gmail.com>
2022-08-24 02:52:41 +0300

    feat: add middle difficulty

---
+++ js/cards.js
@@ -0,0 +1,310 @@
+const cardsData = [
+  {
+    id: 'blue1',
+    src: 'url(/assets/MythicCards/blue/blue1.jpg)',
+    difficulty: 'hard',
+    color:'blue'
+  },
+  {
+    id: 'blue2',
+    src: 'url(/assets/MythicCards/blue/blue2.jpg)',
+    difficulty: 'hard',
+    color:'blue'
+  },
+  {
+    id: 'blue3',
+    src: 'url(/assets/MythicCards/blue/blue3.jpg)',
+    difficulty: 'easy',
+    color:'blue'
+  },
+  {
+    id: 'blue4',
+    src: 'url(/assets/MythicCards/blue/blue4.jpg)',
+    difficulty: 'easy',
+    color:'blue'
+  },
+  {
+    id: 'blue5',
+    src: 'url(/assets/MythicCards/blue/blue5.jpg)',
+    difficulty: 'easy',
+    color:'blue'
+  },
+  {
+    id: 'blue6',
+    src: 'url(/assets/MythicCards/blue/blue6.jpg)',
+    difficulty: 'hard',
+    color:'blue'
+  },
+  {
+    id: 'blue7',
+    src: 'url(/assets/MythicCards/blue/blue7.jpg)',
+    difficulty: 'normal',
+    color:'blue'
+  },
+  {
+    id: 'blue8',
+    src: 'url(/assets/MythicCards/blue/blue8.jpg)',
+    difficulty: 'hard',
+    color:'blue'
+  },
+  {
+    id: 'blue9',
+    src: 'url(/assets/MythicCards/blue/blue9.jpg)',
+    difficulty: 'normal',
+    color:'blue'
+  },
+  {
+    id: 'blue10',
+    src: 'url(/assets/MythicCards/blue/blue10.jpg)',
+    difficulty: 'easy',
+    color:'blue'
+  },
+  {
+    id: 'blue11',
+    src: 'url(/assets/MythicCards/blue/blue11.jpg)',
+    difficulty: 'normal',
+    color:'blue'
+  },
+  {
+    id: 'blue12',
+    src: 'url(/assets/MythicCards/blue/blue11.jpg)',
+    difficulty: 'normal',
+    color:'blue'
+  },
+  {
+    id: 'brown1',
+    src: 'url(/assets/MythicCards/brown/brown1.jpg)',
+    difficulty: 'normal',
+    color:'brown'
+  },
+  {
+    id: 'brown2',
+    src: 'url(/assets/MythicCards/brown/brown2.jpg)',
+    difficulty: 'normal',
+    color:'brown'
+  },
+  {
+    id: 'brown3',
+    src: 'url(/assets/MythicCards/brown/brown3.jpg)',
+    difficulty: 'normal',
+    color:'brown'
+  },
+  {
+    id: 'brown4',
+    src: 'url(/assets/MythicCards/brown/brown4.jpg)',
+    difficulty: 'normal',
+    color:'brown'
+  },
+  {
+    id: 'brown5',
+    src: 'url(/assets/MythicCards/brown/brown5.jpg)',
+    difficulty: 'normal',
+    color:'brown'
+  },
+  {
+    id: 'brown6',
+    src: 'url(/assets/MythicCards/brown/brown6.jpg)',
+    difficulty: 'hard',
+    color:'brown'
+  },
+  {
+    id: 'brown7',
+    src: 'url(/assets/MythicCards/brown/brown7.jpg)',
+    difficulty: 'hard',
+    color:'brown'
+  },
+  {
+    id: 'brown8',
+    src: 'url(/assets/MythicCards/brown/brown8.jpg)',
+    difficulty: 'hard',
+    color:'brown'
+  },
+  {
+    id: 'brown9',
+    src: 'url(/assets/MythicCards/brown/brown9.jpg)',
+    difficulty: 'hard',
+    color:'brown'
+  },
+  {
+    id: 'brown10',
+    src: 'url(/assets/MythicCards/brown/brown10.jpg)',
+    difficulty: 'hard',
+    color:'brown'
+  },
+  {
+    id: 'brown11',
+    src: 'url(/assets/MythicCards/brown/brown11.jpg)',
+    difficulty: 'easy',
+    color:'brown'
+  },
+  {
+    id: 'brown12',
+    src: 'url(/assets/MythicCards/brown/brown12.jpg)',
+    difficulty: 'easy',
+    color:'brown'
+  },
+  {
+    id: 'brown13',
+    src: 'url(/assets/MythicCards/brown/brown13.jpg)',
+    difficulty: 'easy',
+    color:'brown'
+  },
+  {
+    id: 'brown14',
+    src: 'url(/assets/MythicCards/brown/brown14.jpg)',
+    difficulty: 'easy',
+    color:'brown'
+  },
+  {
+    id: 'brown15',
+    src: 'url(/assets/MythicCards/brown/brown15.jpg)',
+    difficulty: 'normal',
+    color:'brown'
+  },
+  {
+    id: 'brown16',
+    src: 'url(/assets/MythicCards/brown/brown16.jpg)',
+    difficulty: 'normal',
+    color:'brown'
+  },
+  {
+    id: 'brown17',
+    src: 'url(/assets/MythicCards/brown/brown17.jpg)',
+    difficulty: 'normal',
+    color:'brown'
+  },
+  {
+    id: 'brown18',
+    src: 'url(/assets/MythicCards/brown/brown18.jpg)',
+    difficulty: 'normal',
+    color:'brown'
+  },
+  {
+    id: 'brown19',
+    src: 'url(/assets/MythicCards/brown/brown19.jpg)',
+    difficulty: 'normal',
+    color:'brown'
+  },
+  {
+    id: 'brown20',
+    src: 'url(/assets/MythicCards/brown/brown20.jpg)',
+    difficulty: 'normal',
+    color:'brown'
+  },
+  {
+    id: 'brown21',
+    src: 'url(/assets/MythicCards/brown/brown21.jpg)',
+    difficulty: 'easy',
+    color:'brown'
+  },
+  {
+    id: 'green1',
+    src: 'url(/assets/MythicCards/green/green1.jpg)',
+    difficulty: 'easy',
+    color:'green'
+  },
+  {
+    id: 'green2',
+    src: 'url(/assets/MythicCards/green/green2.jpg)',
+    difficulty: 'hard',
+    color:'green'
+  },
+  {
+    id: 'green3',
+    src: 'url(/assets/MythicCards/green/green3.jpg)',
+    difficulty: 'hard',
+    color:'green'
+  },
+  {
+    id: 'green4',
+    src: 'url(/assets/MythicCards/green/green4.jpg)',
+    difficulty: 'hard',
+    color:'green'
+  },
+  {
+    id: 'green5',
+    src: 'url(/assets/MythicCards/green/green5.jpg)',
+    difficulty: 'hard',
+    color:'green'
+  },
+  {
+    id: 'green6',
+    src: 'url(/assets/MythicCards/green/green6.jpg)',
+    difficulty: 'hard',
+    color:'green'
+  },
+  {
+    id: 'green7',
+    src: 'url(/assets/MythicCards/green/green7.jpg)',
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green8',
+    src: 'url(/assets/MythicCards/green/green8.jpg)',
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green9',
+    src: 'url(/assets/MythicCards/green/green9.jpg)',
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green10',
+    src: 'url(/assets/MythicCards/green/green10.jpg)',
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green11',
+    src: 'url(/assets/MythicCards/green/green11.jpg)',
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green12',
+    src: 'url(/assets/MythicCards/green/green12.jpg)',
+    difficulty: 'easy',
+    color:'green'
+  },
+  {
+    id: 'green13',
+    src: 'url(/assets/MythicCards/green/green13.jpg)',
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green14',
+    src: 'url(/assets/MythicCards/green/green14.jpg)',
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green15',
+    src: 'url(/assets/MythicCards/green/green15.jpg)',
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green16',
+    src: 'url(/assets/MythicCards/green/green16.jpg)',
+    difficulty: 'easy',
+    color:'green'
+  },
+  {
+    id: 'green17',
+    src: 'url(/assets/MythicCards/green/green17.jpg)',
+    difficulty: 'easy',
+    color:'green'
+  },
+  {
+    id: 'green18',
+    src: 'url(/assets/MythicCards/green/green18.jpg)',
+    difficulty: 'easy',
+    color:'green'
+  }
+]
+
+export default cardsData
--- js/script.js
+++ js/script.js
@@ -17,16 +17,28 @@ const dot9 = document.getElementById('dot9');
 
 import ancientsData from './ancients.js';
 
-function setDots(ancient) {
-  dot1.textContent = ancientsData[ancient].firstStage.greenCards;
-  dot2.textContent = ancientsData[ancient].firstStage.brownCards;
-  dot3.textContent = ancientsData[ancient].firstStage.blueCards;
-  dot4.textContent = ancientsData[ancient].secondStage.greenCards;
-  dot5.textContent = ancientsData[ancient].secondStage.brownCards;
-  dot6.textContent = ancientsData[ancient].secondStage.blueCards;
-  dot7.textContent = ancientsData[ancient].thirdStage.greenCards;
-  dot8.textContent = ancientsData[ancient].thirdStage.brownCards;
-  dot9.textContent = ancientsData[ancient].thirdStage.blueCards;
+let ancient, diffLevel;
+let dot1Num, dot2Num, dot3Num, dot4Num, dot5Num, dot6Num, dot7Num, dot8Num, dot9Num;
+
+function setDots(ancientNum) {
+  dot1Num = ancientsData[ancientNum].firstStage.greenCards;
+  dot1.textContent = dot1Num;
+  dot2Num = ancientsData[ancientNum].firstStage.brownCards;
+  dot2.textContent = dot2Num;
+  dot3Num = ancientsData[ancientNum].firstStage.blueCards;
+  dot3.textContent = dot3Num;
+  dot4Num = ancientsData[ancientNum].secondStage.greenCards
+  dot4.textContent = dot4Num;
+  dot5Num = ancientsData[ancientNum].secondStage.brownCards;
+  dot5.textContent = dot5Num;
+  dot6Num = ancientsData[ancientNum].secondStage.blueCards;
+  dot6.textContent = dot6Num;
+  dot7Num = ancientsData[ancientNum].thirdStage.greenCards;
+  dot7.textContent = dot7Num;
+  dot8Num = ancientsData[ancientNum].thirdStage.brownCards
+  dot8.textContent = dot8Num;
+  dot9Num = ancientsData[ancientNum].thirdStage.blueCards;
+  dot9.textContent = dot9Num;
 }
 
 Azathoth.addEventListener('click', function() {
@@ -35,6 +47,7 @@ Azathoth.addEventListener('click', function() {
   IogSothoth.classList.remove('active');
   ShubNiggurath.classList.remove('active');
   setDots(0);
+  ancient = 'azathoth';
 })
 Cthulthu.addEventListener('click', function() {
   Cthulthu.classList.add('active');
@@ -42,6 +55,8 @@ Cthulthu.addEventListener('click', function() {
   IogSothoth.classList.remove('active');
   ShubNiggurath.classList.remove('active');
   setDots(1);
+  ancient = 'cthulhu';
+
 })
 IogSothoth.addEventListener('click', function() {
   IogSothoth.classList.add('active');
@@ -49,6 +64,7 @@ IogSothoth.addEventListener('click', function() {
   Cthulthu.classList.remove('active');
   ShubNiggurath.classList.remove('active');
   setDots(2);
+  ancient = 'iogSothoth';
 })
 ShubNiggurath.addEventListener('click', function() {
   ShubNiggurath.classList.add('active');
@@ -56,6 +72,7 @@ ShubNiggurath.addEventListener('click', function() {
   Cthulthu.classList.remove('active');
   IogSothoth.classList.remove('active');
   setDots(3);
+  ancient = 'shubNiggurath';
 })
 
 // выбор уровня сложности
@@ -66,14 +83,13 @@ const btnMiddle = document.querySelector('.button-middle');
 const btnDifficult = document.querySelector('.button-difficult');
 const btnVeryDifficult = document.querySelector('.button-very-difficult');
 
-const btnShuffle = document.querySelector('.button-very-shuffle');
-
 btnVeryEasy.addEventListener('click', function() {
   btnVeryEasy.classList.add('active');
   btnEasy.classList.remove('active');
   btnMiddle.classList.remove('active');
   btnDifficult.classList.remove('active');
   btnVeryDifficult.classList.remove('active');
+  diffLevel = 'very-easy';
 })
 btnEasy.addEventListener('click', function() {
   btnEasy.classList.add('active');
@@ -81,6 +97,7 @@ btnEasy.addEventListener('click', function() {
   btnMiddle.classList.remove('active');
   btnDifficult.classList.remove('active');
   btnVeryDifficult.classList.remove('active');
+  diffLevel = 'easy';
 })
 btnMiddle.addEventListener('click', function() {
   btnMiddle.classList.add('active');
@@ -88,6 +105,7 @@ btnMiddle.addEventListener('click', function() {
   btnEasy.classList.remove('active');
   btnDifficult.classList.remove('active');
   btnVeryDifficult.classList.remove('active');
+  diffLevel = 'very-easy';
 })
 btnDifficult.addEventListener('click', function() {
   btnDifficult.classList.add('active');
@@ -103,3 +121,110 @@ btnVeryDifficult.addEventListener('click', function() {
   btnMiddle.classList.remove('active');
   btnDifficult.classList.remove('active');
 })
+
+// замешивание колоды
+
+const btnShuffle = document.querySelector('.button-shuffle');
+
+
+
+btnShuffle.addEventListener('click', shuffle);
+
+
+let arrCards = [];
+
+function shuffle() {
+
+
+  let arrBlue = [];
+  let arrBrown = [];
+  let arrGreen = [];
+
+  let arrFirstStage = [];
+  let arrSecondStage = [];
+  let arrThirdStage = [];
+
+cardsData.forEach(element => {
+  if (element.color === 'blue') {
+    arrBlue.push(element.src);
+  } else if (element.color === 'brown') {
+    arrBrown.push(element.src);
+  } else {
+    arrGreen.push(element.src);
+  }
+})
+
+
+function cardsShuffle(a, b) {
+  return 0.5 - Math.random();
+}
+
+arrBlue.sort(cardsShuffle);
+arrBrown.sort(cardsShuffle);
+arrGreen.sort(cardsShuffle);
+
+arrThirdStage = arrBlue.slice(0, dot9Num).concat(arrBrown.slice(0, dot8Num)).concat(arrGreen.slice(0, dot7Num));
+console.log(arrThirdStage);
+arrSecondStage = arrBlue.slice(dot9Num, dot9Num + dot6Num).concat(arrBrown.slice(dot8Num, parseInt(dot8Num) + parseInt(dot5Num))).concat(arrGreen.slice(dot7Num, dot7Num + dot4Num));
+console.log(arrSecondStage);
+arrFirstStage = arrBlue.slice(dot9Num + dot6Num, dot9Num + dot6Num + dot3Num).concat(arrBrown.slice(dot8Num + dot5Num, dot8Num + dot5Num + dot2Num)).concat(arrGreen.slice(dot7Num + dot4Num, dot7Num + dot4Num + dot1Num));
+console.log(arrFirstStage);
+
+
+arrCards = arrThirdStage.sort(cardsShuffle).concat(arrSecondStage.sort(cardsShuffle)).concat(arrFirstStage.sort(cardsShuffle));
+console.log(arrCards);
+}
+
+
+// показ карт и обновление трекера
+
+import cardsData from './cards.js';
+const lastCard = document.querySelector('.last-card');
+const btnShowCard = document.querySelector('.card-back');
+
+btnShowCard.addEventListener('click', function() {
+  if (arrCards.length > 0) {
+    lastCard.style.backgroundImage = arrCards[(arrCards.length - 1)];
+    if ((arrCards[(arrCards.length - 1)].substring(24, 26)) === 'gr') {
+      if (dot1.textContent != 0) {
+        dot1.textContent--;
+      } else if (dot4.textContent != 0) {
+        dot4.textContent--;
+      } else if (dot7.textContent != 0) {
+        dot7.textContent--;
+      }
+    } else if ((arrCards[(arrCards.length - 1)].substring(24, 26)) === 'br') {
+      if (dot2.textContent != 0) {
+        dot2.textContent--;
+      } else if (dot5.textContent != 0) {
+        dot5.textContent--;
+      } else if (dot8.textContent != 0) {
+        dot8.textContent--;
+      }
+    } else {
+      if (dot3.textContent != 0) {
+        dot3.textContent--;
+      } else if (dot6.textContent != 0) {
+        dot6.textContent--;
+      } else if (dot9.textContent != 0) {
+        dot9.textContent--;
+      }
+    }
+    arrCards.pop();
+  } else {
+    Azathoth.classList.remove('active');
+    Cthulthu.classList.remove('active');
+    IogSothoth.classList.remove('active');
+    ShubNiggurath.classList.remove('active');
+    
+    btnVeryEasy.classList.remove('active');
+    btnEasy.classList.remove('active');
+    btnMiddle.classList.remove('active');
+    btnDifficult.classList.remove('active');
+    btnVeryDifficult.classList.remove('active');
+
+    lastCard.style.backgroundImage = '';
+    alert('Выберите Древнего, уровень сложности и замешайте колоду, пожалуйста.');
+  }
+})
+
